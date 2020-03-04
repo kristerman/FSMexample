ada39cc128bcf1ce29ce86e1ecfa38943ab0d89c
Christos Karamitsos <ckaramitsos@mcoms.com>
2020-03-04 17:46:26 +0200

    Remove commented code

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@ typedef std::chrono::high_resolution_clock Clock;
 
 
 void event_generation() {
-    //State(StateType::Start);
-    //auto intstr = Instruction(static_cast<size_t>(0));
     while(true) {
         auto instr = Instruction(static_cast<size_t>(rand()));
         this_thread::sleep_for(chrono::seconds(1));
